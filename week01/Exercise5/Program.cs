3b137f251875498443000a064c2b23fae71fae86
mynor.aguirre.0211 <mynor.aguirre.0211@gmail.com>
2026-05-13 22:17:46 -0600

    exercise 5 week 01

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        static void DisplayWelcome ()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+        DisplayWelcome();
+
+        static string PromptUserName ()
+        {
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+
+            return userName;
+        }
+
+        string name = PromptUserName();
+
+        static int promptUserNumber ()
+        {
+            Console.Write("Please enter a number: ");
+            string userResponse = Console.ReadLine();
+            int userNumber = int.Parse(userResponse);
+
+            return userNumber;
+        }
+
+        int number = promptUserNumber();
+
+        static int SquareNumber (int number)
+        {
+            int squaredNumber = number * number;
+
+            return squaredNumber;
+        }
+        int squared = SquareNumber(number);
+
+        static void DisplayResult (string name, int squared)
+        {
+            Console.WriteLine($"{name}, the square of your number is: {squared}");
+        }
+        DisplayResult(name, squared);
     }
 }
