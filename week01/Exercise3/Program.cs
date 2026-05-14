0cd01277707b6b091bf0b7e3ebd768c1eaffbf80
mynor.aguirre.0211 <mynor.aguirre.0211@gmail.com>
2026-05-13 21:32:09 -0600

    3rd excercise core requirements

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        while (guess != number)
+        {   
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+       
+            if (guess == number)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+       }
+        
     }
 }
